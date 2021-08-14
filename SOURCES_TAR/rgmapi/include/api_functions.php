72fe2f36faa1231cd594f986e9ecf9338b91b304
Vincent FRICOU <vincent@fricouv.eu>
2021-08-14 19:07:17 +0200

    fix: trailing spaces
    fix(sonar): fix literal false comparison
    
    fix: perform code reformating
    
    fix(sonar): remove useless variable for checkAuthToken
    
    fix(sonar): issues on missing curly braces
    
    fix(sonar): merge if statements
    
    fix(sonar): boolean comparison statements
    
    fix(sonar): multiples OR statements characters
    
    fix(sonar): optimize function return
    
    fix(ObjectManager): default export job name

--- SOURCES_TAR/rgmapi/include/api_functions.php
+++ SOURCES_TAR/rgmapi/include/api_functions.php
@@ -1,71 +1,74 @@
 <?php
-/*
-  RGMAPI 
-  Route calls
+  /*
+    RGMAPI
+    Route calls
 
-  Copyleft 2018 RGM
-  Author: BU DCA Team based on Adrien van den Haak initial work (https://github.com/EyesOfNetworkCommunity/rgmapi)
-*/
+    Copyleft 2018 RGM
+    Author: BU DCA Team based on Adrien van den Haak initial work (https://github.com/EyesOfNetworkCommunity/rgmapi)
+  */
 
-const ACL_NOAUTH     = 0b00001; // authentication not required (token *not* needed)
-const ACL_READONLY   = 0b00010; // action restricted to read-only
-const ACL_1T_TOKEN   = 0b00100; // one-time token authorized for this action
-const ACL_1T_NOCLEAR = 0b00100; // don't clear one-time token after call
-const ACL_ADMIN      = 0b10000; // action restricted to admin users
+  const ACL_NOAUTH = 0b00001; // authentication not required (token *not* needed)
+  const ACL_READONLY = 0b00010; // action restricted to read-only
+  const ACL_1T_TOKEN = 0b00100; // one-time token authorized for this action
+  const ACL_1T_NOCLEAR = 0b00100; // don't clear one-time token after call
+  const ACL_ADMIN = 0b10000; // action restricted to admin users
 
-function getUserByUsername($username) {
+  function getUserByUsername($username)
+  {
     global $database_rgmweb;
-    
+
     return sqlrequest(
-        $database_rgmweb,
-        "SELECT U.user_id as user_id, U.user_name as user_name, U.user_passwd as user_passwd, U.user_type as user_type,
+      $database_rgmweb,
+      "SELECT U.user_id as user_id, U.user_name as user_name, U.user_passwd as user_passwd, U.user_type as user_type,
 		U.user_limitation as user_limitation, R.tab_1 as readonly, R.tab_2 as operator, R.tab_6 as admin
 		FROM users as U left join groups as G on U.group_id = G.group_id left join groupright as R on R.group_id = G.group_id
 		WHERE U.user_name = ?",
-        false,
-		array($username)
-	);
-}
+      false,
+      array($username)
+    );
+  }
 
-/*---HTTP Response---*/
-function getJsonResponse($response, $code, $array = null) {	
-	global $app;
+  /*---HTTP Response---*/
+  function getJsonResponse($response, $code, $array = null)
+  {
+    global $app;
 
     // RGM API version is the concatenation on Slim framework version *and* RGM API level revision
     $codeMessage = $response->getMessageForCode($code);
     $arrayHeader = array(
-        "slim_version" => \Slim\Slim::VERSION,
-        "rgmapi_version" => '1.0',
-        "http_code" => $codeMessage
+      "slim_version" => \Slim\Slim::VERSION,
+      "rgmapi_version" => '1.0',
+      "http_code" => $codeMessage
     );
     $arrayMerge = array_merge($arrayHeader, $array);
 
     $jsonResponse = json_encode($arrayMerge, JSON_PRETTY_PRINT);
     $jsonResponseWithHeader = $jsonResponse;
 
-	$app->response->headers->set('Content-Type', 'application/json');
-	$app->response->setStatus($codeMessage);
+    $app->response->headers->set('Content-Type', 'application/json');
+    $app->response->setStatus($codeMessage);
 
     return $jsonResponseWithHeader;
-}
+  }
 
-function constructResponse($response, $logs, $authenticationValid = false) {
+  function constructResponse($response, $logs, $authenticationValid = false)
+  {
     //Only if API keys match
     if ($authenticationValid) {
-        try {
-            $array = array("result" => $logs);
-            $result = getJsonResponse($response, "200", $array);
-            echo $result;
-        } catch(PDOException $e) {
-            $array = array("error" => $e->getMessage());
-            $result = getJsonResponse($response, "400", $array);
-            echo $result;
-        }
-    } else {
-        $array = array("status" => "unauthorized");
-        $result = getJsonResponse($response, "401", $array);
+      try {
+        $array = array("result" => $logs);
+        $result = getJsonResponse($response, "200", $array);
         echo $result;
+      } catch (PDOException $e) {
+        $array = array("error" => $e->getMessage());
+        $result = getJsonResponse($response, "400", $array);
+        echo $result;
+      }
+    } else {
+      $array = array("status" => "unauthorized");
+      $result = getJsonResponse($response, "401", $array);
+      echo $result;
     }
-}
+  }
 
 ?>
--- SOURCES_TAR/rgmapi/include/rgmauth.php
+++ SOURCES_TAR/rgmapi/include/rgmauth.php
@@ -1,30 +1,31 @@
 <?php
-/**
- * RGMAPI - authentication calls
- * Copyright (c) 2019 SCC France - RGM Team
- * Eric Belhomme <ebelhomme@fr.scc.com>
- * 
- * LICENCE :
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+  /**
+   * RGMAPI - authentication calls
+   * Copyright (c) 2019 SCC France - RGM Team
+   * Eric Belhomme <ebelhomme@fr.scc.com>
+   *
+   * LICENCE :
+   * This program is free software; you can redistribute it and/or
+   * modify it under the terms of the GNU General Public License
+   * as published by the Free Software Foundation; either version 2
+   * of the License, or (at your option) any later version.
+   * This program is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+   */
 
 
-/**
- * @brief   Generates a unique token and session id
- * @details Generates a unique token and session ID when invoked. It does @b not
- *          handle any database related operation.
- *          This function is not exported throught Slim routing handlers
- * @param   $username The username used to create the token
- * @return  an array composed of @b session and @b token keys 
- */
-function _genToken($username) {
+  /**
+   * @brief   Generates a unique token and session id
+   * @details Generates a unique token and session ID when invoked. It does @b not
+   *          handle any database related operation.
+   *          This function is not exported throught Slim routing handlers
+   * @param   $username The username used to create the token
+   * @return  an array composed of @b session and @b token keys
+   */
+  function _genToken($username)
+  {
     // gen token randomly
     $ret = array();
     global $rgm_session;
@@ -33,47 +34,49 @@ function _genToken($username) {
     $salt = mcrypt_create_iv(22, MCRYPT_DEV_URANDOM);
     $salt = base64_encode($salt);
     $salt = str_replace('+', '.', $salt);
-    $ret['token'] = hash('sha256', crypt($username, '$2y$10$'.$salt.'$') . $_SERVER['SERVER_ADDR']); // NOSONAR
+    $ret['token'] = hash('sha256', crypt($username, '$2y$10$' . $salt . '$') . $_SERVER['SERVER_ADDR']); // NOSONAR
     return $ret;
-}
+  }
 
-/**
- * @brief   Generates a one-time token
- * @details When invoked, this function generates a new token and insert it into
- *          the @b sessions table
- * @param   none
- * @return  nothing
- */
-function _genOneTimeToken() {
+  /**
+   * @brief   Generates a one-time token
+   * @details When invoked, this function generates a new token and insert it into
+   *          the @b sessions table
+   * @param none
+   * @return  nothing
+   */
+  function _genOneTimeToken()
+  {
     global $database_rgmweb;
     $tokeninfo = _genToken('one-time-token');
     sqlrequest(
-        $database_rgmweb,
-        "INSERT INTO sessions (session_id, session_token, session_type, creation_epoch, update_epoch) VALUES (?, ?, 3, UNIX_TIMESTAMP(), UNIX_TIMESTAMP())",
-        true,
-        array($tokeninfo['session'], $tokeninfo['token'])
+      $database_rgmweb,
+      "INSERT INTO sessions (session_id, session_token, session_type, creation_epoch, update_epoch) VALUES (?, ?, 3, UNIX_TIMESTAMP(), UNIX_TIMESTAMP())",
+      true,
+      array($tokeninfo['session'], $tokeninfo['token'])
     );
-}
+  }
 
-/**
- * @brief   check token validity
- * @details check the validity of the token passed as argument. It return an array with token details
- * @param   $request    a pointer to Slimm::request class with current context
- * @param   $token      the token to check
- * @return  $tokenInfo  an array with token details
- */
-function checkAuthTokenValidity($request, $token) {
+  /**
+   * @brief   check token validity
+   * @details check the validity of the token passed as argument. It return an array with token details
+   * @param   $request    a pointer to Slimm::request class with current context
+   * @param   $token      the token to check
+   * @return  $tokenInfo  an array with token details
+   */
+  function checkAuthTokenValidity($token)
+  {
     global $database_rgmweb;
     global $rgmauth_ttl;
 
     $tokenInfo = array(
-        'session_id' => 'None',
-        'creation_epoch' => 0,
-        'status' => 'unauthorized',
-        'username' => ''
+      'session_id' => 'None',
+      'creation_epoch' => 0,
+      'status' => 'unauthorized',
+      'username' => ''
     );
     $now = time();
-    
+
     // clean expired tokens
     // FIXME: Alex: j'émets une réserve sur cette suppression, j'ai un doute que le calcul soit identique
     // à ce qu'on fait ailleurs. UNIX_TIMESTAMP() ==> à vérifier
@@ -82,47 +85,48 @@ function checkAuthTokenValidity($request, $token) {
     // Ca sert quand même de trace, on peut purger tout ce qui a plus d'un an par exemple mais pas des trucs échus
     // depuis une journée.
     sqlrequest(
-        $database_rgmweb,
-        "DELETE FROM sessions WHERE creation_epoch < ? AND session_type IN (2, 3)",
-        false,
-        array($now - $rgmauth_ttl)
+      $database_rgmweb,
+      "DELETE FROM sessions WHERE creation_epoch < ? AND session_type IN (2, 3)",
+      false,
+      array($now - $rgmauth_ttl)
     );
 
     // try to find an existing token
     $stmt = sqlrequest(
-        $database_rgmweb,
-        "SELECT session_id, creation_epoch, user_id, session_type FROM sessions WHERE session_type IN (2, 3) AND session_token = ?",
-        false,
-        array($token)
+      $database_rgmweb,
+      "SELECT session_id, creation_epoch, user_id, session_type FROM sessions WHERE session_type IN (2, 3) AND session_token = ?",
+      false,
+      array($token)
     );
     $sql_raw = mysqli_fetch_row($stmt);
 
     if (count($sql_raw) == 4) {
-        $tokenInfo['session_id'] = $sql_raw[0];
-        $tokenInfo['creation_epoch'] = $sql_raw[1];
-        $tokenInfo['status'] = "authorized";
-        $session_type = $sql_raw[3];
-        switch ($session_type) {
-            case 2:
-                $stmt = sqlrequest($database_rgmweb, "SELECT user_name FROM users WHERE user_id = ?", false, array($sql_raw[2]));
-                $tokenInfo['username'] = mysqli_result($stmt, 0, "user_name");
-                break;
-            case 3:
-                $tokenInfo['username'] = 'one-time-token';
-                break;
-            default:
-                error_log("checkAuthTokenValidity() : unknown session_type=" . $session_type);
-                break;
-        }
+      $tokenInfo['session_id'] = $sql_raw[0];
+      $tokenInfo['creation_epoch'] = $sql_raw[1];
+      $tokenInfo['status'] = "authorized";
+      $session_type = $sql_raw[3];
+      switch ($session_type) {
+        case 2:
+          $stmt = sqlrequest($database_rgmweb, "SELECT user_name FROM users WHERE user_id = ?", false, array($sql_raw[2]));
+          $tokenInfo['username'] = mysqli_result($stmt, 0, "user_name");
+          break;
+        case 3:
+          $tokenInfo['username'] = 'one-time-token';
+          break;
+        default:
+          error_log("checkAuthTokenValidity() : unknown session_type=" . $session_type);
+          break;
+      }
     }
 
     return $tokenInfo;
-}
+  }
 
-/**
- * @brief   registers a user (provided username/password) and returns an auth token
- */
-function getAuthToken() {
+  /**
+   * @brief   registers a user (provided username/password) and returns an auth token
+   */
+  function getAuthToken()
+  {
     global $database_rgmweb;
     $request = \Slim\Slim::getInstance()->request();
     $response = \Slim\Slim::getInstance()->response();
@@ -134,76 +138,81 @@ function getAuthToken() {
     $password = RgmSession::getHashedPassword($request->get('password')); // NOSONAR
     
     if ($userintable = getUserByUsername($username)) {
-        $user_id = mysqli_result($userintable, 0, "user_id");
-        $user_right = mysqli_result($userintable, 0, "readonly");
-        $user_type = mysqli_result($userintable, 0, "user_type");
-        $userpasswd = mysqli_result($userintable, 0, "user_passwd");
+      $user_id = mysqli_result($userintable, 0, "user_id");
+      $user_right = mysqli_result($userintable, 0, "readonly");
+      $user_type = mysqli_result($userintable, 0, "user_type");
+      $userpasswd = mysqli_result($userintable, 0, "user_passwd");
     } else {
+      $array = array("message" => "Wrong credentials (invalid username or password)");
+      $result = getJsonResponse($response, "401", $array);
+      echo $result;
+      return;
+    }
+
+    // access to API require user with admin privs
+    if ($user_type != "1" && $user_right == "1") {
+      //IF match the hashed password
+      if ($userpasswd == $password) {
+        $tokeninfo = _genToken($username);
+        sqlrequest(
+          $database_rgmweb,
+          "INSERT INTO `sessions` (session_id, user_id, session_type, session_token, creation_epoch) VALUES (?, ?, 2, ?, ?)",
+          false,
+          array($tokeninfo['session'], $user_id, $tokeninfo['token'], time())
+        );
+        $array = array("RGMAPI_TOKEN" => $tokeninfo['token']);
+        $result = getJsonResponse($response, "200", $array);
+        echo $result;
+      } else {
         $array = array("message" => "Wrong credentials (invalid username or password)");
         $result = getJsonResponse($response, "401", $array);
         echo $result;
-        return;
-    }
-    
-    // access to API require user with admin privs
-    if ($user_type != "1" && $user_right == "1") {
-        //IF match the hashed password
-        if($userpasswd == $password) {
-            $tokeninfo = _genToken($username);
-            sqlrequest(
-                $database_rgmweb,
-                "INSERT INTO `sessions` (session_id, user_id, session_type, session_token, creation_epoch) VALUES (?, ?, 2, ?, ?)",
-                false,
-                array($tokeninfo['session'], $user_id, $tokeninfo['token'], time())
-            );
-            $array = array("RGMAPI_TOKEN" => $tokeninfo['token']);
-            $result = getJsonResponse($response, "200", $array);
-            echo $result;
-        } else {
-            $array = array("message" => "Wrong credentials (invalid username or password)");
-            $result = getJsonResponse($response, "401", $array);
-            echo $result;
-        }  
+      }
     } else {
-        $array = array("message" => "User not allowed to get RGMAPI token");
-        $result = getJsonResponse($response, "401", $array);
-        echo $result;
+      $array = array("message" => "User not allowed to get RGMAPI token");
+      $result = getJsonResponse($response, "401", $array);
+      echo $result;
     }
-}
+  }
 
-/**
- * @brief   find token in Slim::request parameters (either in URI params and/or in HTTP headers)
- */
-function getTokenParameter($request, $body = null) { 
+  /**
+   * @brief   find token in Slim::request parameters (either in URI params and/or in HTTP headers)
+   */
+  function getTokenParameter($request, $body = null)
+  {
     // Search for token parameter passed as variable or in http headers
     $token = '';
     if ($header = $request->headers->get('token')) {
-        $token = $header;
+      $token = $header;
     }
-    if ( $var = $request->get('token')) {
-        $token = $var;
+    if ($var = $request->get('token')) {
+      $token = $var;
     }
     if (isset($body['token'])) {
-        $token = $body['token'];
+      $token = $body['token'];
     }
     return $token;
-}
+  }
 
-function checkAuthToken($token) {
+  function checkAuthToken($token)
+  {
     $request = \Slim\Slim::getInstance()->request();
     $response = \Slim\Slim::getInstance()->response();
     $token = getTokenParameter($request);
     $httpcode = '401';
-    $tokenInfo = checkAuthTokenValidity($request, $token);
-    if ($tokenInfo['status'] == 'authorized')
-        $httpcode = '200';
-    
+    $tokenInfo = checkAuthTokenValidity($token);
+    if ($tokenInfo['status'] == 'authorized') {
+      $httpcode = '200';
+    }
+
     $result = getJsonResponse($response, $httpcode, $tokenInfo);
     echo $result;
-}
+  }
 
-function clearOneTimeToken($token) {
+  function clearOneTimeToken($token)
+  {
     global $database_rgmweb;
-    sqlrequest( $database_rgmweb, "DELETE FROM sessions WHERE session_type = 3 AND session_token = ?", false, array($token));
-}
+    sqlrequest($database_rgmweb, "DELETE FROM sessions WHERE session_type = 3 AND session_token = ?", false, array($token));
+  }
+
 ?>
